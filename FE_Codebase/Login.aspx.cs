b3689caa481a501b3a6cc58c291a5bc4671c78ae
Tanuj-Koduri <tanuj.koduri@peopletech.com>
2024-11-21 11:17:40 -0800

    Modernizing code

---
+++ FE_Codebase/Login.aspx.cs
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+```
+
+2. Add a language attribute to the html tag:
+```html
+<html lang="en">
+```
+
+3. Add viewport meta tag for better responsive design:
+```html
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
